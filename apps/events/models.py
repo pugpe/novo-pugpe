11055bb2f21091eb24240cf04bde0bcd567559ee
Renato Oliveira <gh@renatooliveira.me>
2023-03-18 17:46:50 -0300

    create venue model

--- apps/events/models.py
+++ apps/events/models.py
@@ -1,12 +1,28 @@
 from django.db import models
 
 
+class Venue(models.Model):
+    name = models.CharField(max_length=255)
+    address_line_1 = models.CharField(max_length=255)
+    address_line_2 = models.CharField(max_length=255, blank=True)
+    neighborhood = models.CharField(max_length=255)
+    city = models.CharField(max_length=255)
+    state = models.CharField(max_length=255)
+    instructions = models.TextField(blank=True)
+
+    def __str__(self):
+        return self.name
+
+
 class Event(models.Model):
     name = models.CharField(max_length=255)
     slug = models.SlugField(max_length=255)
     description = models.TextField()
     start_date = models.DateTimeField()
-    end_date = models.DateTimeField(null=True)
+    end_date = models.DateTimeField(null=True, blank=True)
+    venue = models.ForeignKey(
+        Venue, on_delete=models.CASCADE, null=True, blank=True, related_name="events"
+    )
 
     def __str__(self):
         return self.name
--- apps/events/tests/test_models.py
+++ apps/events/tests/test_models.py
@@ -1,6 +1,6 @@
 import pytest
 
-from apps.events.models import Event
+from apps.events.models import Event, Venue
 
 
 @pytest.mark.django_db
@@ -8,7 +8,30 @@ def test_can_create_event():
     e = Event.objects.create(
         name="Test Event",
         description="This is a test event",
-        start_date="2023-04-01",
+        start_date="2023-04-01 10:00:00 -03:00",
     )
 
-    assert e.name == "Test Event"
+    assert str(e) == "Test Event"
+
+
+@pytest.mark.django_db
+def test_event_should_have_a_venue():
+    v = Venue.objects.create(
+        name="Test Venue",
+        address_line_1="Universidade Federal de Pernambuco, Centro de Informática",
+        address_line_2="Anfiteatro",
+        neighborhood="Cidade Universitária",
+        city="Recife",
+        state="PE",
+        instructions="Entrar pela porta principal",
+    )
+
+    e = Event.objects.create(
+        name="Test Event",
+        description="This is a test event",
+        start_date="2023-04-01 10:00:00 -03:00",
+        venue=v,
+    )
+
+    assert str(e) == "Test Event"
+    assert e.venue == v
